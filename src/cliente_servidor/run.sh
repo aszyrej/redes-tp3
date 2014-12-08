a0e0bc4666919ac71d6533ee7ab61c33f993ed92
Martin Fixman <martinfixman@gmail.com>
2014-12-08 12:46:42 -0300

    Better run.sh script.

--- src/cliente_servidor/run.sh
+++ src/cliente_servidor/run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-sudo ipython server.py 1> /dev/null &
-sudo ipython client.py 
+
+ipython=`which ipython2 2> /dev/null || which ipython`
+
+if [ `id -u` != 0 ] ; then
+	echo "This script needs to be run as root"
+	exit
+fi
+
+sudo $ipython server.py 1> /dev/null &
+sudo $ipython client.py 
