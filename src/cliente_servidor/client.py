9679c2dda8f4ef87e2e2a7cb72d667d8aee5a6b3
Martin Fixman <martinfixman@gmail.com>
2014-12-08 13:40:13 -0300

    Ultimo experimento con codigo mas simple y lindo.

--- src/cliente_servidor/client.py
+++ src/cliente_servidor/client.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+#Tampoco vi que hayan estimado el RTT real, lo cual es importante para contrastar con lo encontradi -*- coding: utf-8 -*-
 
 try:
     from ptc import Socket, SHUT_WR
@@ -155,74 +155,57 @@ def perdidos_vs_alpha_vs_beta():
     pylab.savefig('ret_colormap', format='pdf', orientation='landscape')
     pylab.clf()
 
-def congestion_subita():
-    print "CONGESTION"
+class congestion_subida:
+    def __init__(self):
+        self.rtos = []
+        self.rtts = []
 
-    time.sleep(1)
-
-    alpha, beta  = 0., 0.
-    delay, proba = 25. , 0.05
-    verbose = False
-    rtos = []
-    rtts = []
-
-    with Socket(alpha, beta, proba, delay, verbose ) as client_sock:
-        client_sock.connect((SERVER_IP, SERVER_PORT), timeout=5)
-
-        #Veo el RTO
-        client_sock.alumnos_print_rto()       
-
+    def burst(self, times = 150):
         for i in xrange(150):
-
-            client_sock.send(to_send)
+            self.client_sock.send(to_send)
 
             if i%5 == 0:
                 # Cada 5 envios veo el RTO luego de
                 # asegurarme que llegaron todos los paquetes
                 time.sleep(0.6)
-                rto, rtt = client_sock.alumnos_print_rto()       
-                rtos += [rto]
-                rtts += [rtt]
+                rto, rtt = self.client_sock.alumnos_print_rto()       
+                self.rtos += [rto]
+                self.rtts += [rtt]
+
+    def run(self, alpha = 0, beta = 0, delay = 25, proba = .05, delay_final = 50, verbose=False):
+        print "Calculando parametros con congestion"
+        with Socket(alpha, beta, proba, delay, verbose) as self.client_sock:
+            self.client_sock.connect((SERVER_IP, SERVER_PORT), timeout=5)
+
+            self.burst()
+            self.client_sock.alumnos_print_rto()       
+                    
+            print "Haciendo delay"
+            self.client_sock.alumnos_change_delay(delay_final)
+            self.burst()
                 
-        print "Ahora DELAY DE LA MUERTE"
-        
-        client_sock.alumnos_change_delay(50.)
+            #El ultimo mensaje tiene que llegar si o si
+            self.client_sock.send(end)
+            self.write_report()
 
-        for i in xrange(150):
+    def write_report(self, output='congestion.pdf'):
+        paquetes = [i*5 for i in xrange(60)]
+        pylab.plot(paquetes, self.rtos)
+        pylab.plot(paquetes, self.rtts)   
 
-            client_sock.send(to_send)
-
-            if i%5 == 0:
-                # Cada 5 envios veo el RTO luego de
-                # asegurarme que llegaron todos los paquetes
-                time.sleep(0.6)
-                rto, rtt = client_sock.alumnos_print_rto()       
-                rtos += [rto]
-                rtts += [rtt]
-            
-        #El ultimo mensaje tiene que llegar si o si
-        client_sock.send(end)
-
-    paquetes = [i*5 for i in xrange(60)]
-    pylab.plot(paquetes, rtos)
-    pylab.plot(paquetes, rtts)   
-
-    pylab.xlabel('Cantidad Paquetes Enviados')
-    pylab.ylabel('RTO (ticks)')
-    pylab.title ('RTO vs Numero de Paquetes')
-    pylab.legend(['rto','rtt'])
-    pylab.tight_layout()
-    pylab.savefig('congestion', format='pdf', orientation='landscape')
-    pylab.clf()
-
-
-    
+        pylab.xlabel('Cantidad Paquetes Enviados')
+        pylab.ylabel('RTO (ticks)')
+        pylab.title ('RTO vs Numero de Paquetes')
+        pylab.legend(['rto','rtt'])
+        pylab.tight_layout()
+        pylab.savefig(output, format='pdf', orientation='landscape')
+        pylab.clf()
 
 def main():
 #   n_vs_rto()
 #   rto_vs_alpha_vs_beta()
 #   perdidos_vs_alpha_vs_beta()
-   congestion_subita()
+   congestion_subida().run()
 
 if __name__ == "__main__":
     main()
